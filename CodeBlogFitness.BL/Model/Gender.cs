d8d374d8ffed1d79bf515ad2e918614bb4f0705e
DJVERTER <creativekira1982@gmail.com>
2022-10-20 20:49:16 +0300

    Create User Model

---
+++ CodeBlogFitness.BL/Model/Gender.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace CodeBlogFitness.BL.Model
+{
+    /// <summary>
+    /// Gender.
+    /// </summary>
+    public class Gender
+    {
+        /// <summary>
+        /// Name.
+        /// </summary>
+        public string Name { get; }
+
+        /// <summary>
+        /// Create new gender.
+        /// </summary>
+        /// <param name="name"> Gender name. </param>
+        public Gender(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException("Gender name does not be empty or null", nameof(name));
+            }
+
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
---
+++ CodeBlogFitness.BL/Model/User.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace CodeBlogFitness.BL.Model
+{
+    /// <summary>
+    /// User.
+    /// </summary>
+    public class User
+    {
+        #region Properties
+        /// <summary>
+        /// Name.
+        /// </summary>
+        public string Name { get; }
+        /// <summary>
+        /// Gender.
+        /// </summary>
+        public Gender Gender { get; } 
+        /// <summary>
+        /// BirthDate.
+        /// </summary>
+        public DateTime BirthDate { get; }
+        /// <summary>
+        /// Weight.
+        /// </summary>
+        public double Weight { get; set; }
+        /// <summary>
+        /// Height.
+        /// </summary>
+        public double Height { get; set; }
+        #endregion
+
+        /// <summary>
+        /// Create new user.
+        /// </summary>
+        /// <param name="name"> Name. </param>
+        /// <param name="gender"> Gender. </param>
+        /// <param name="birthDate"> BirthDate. </param>
+        /// <param name="weight"> Weight. </param>
+        /// <param name="height"> Height. </param>
+        public User(string name,
+                    Gender gender,
+                    DateTime birthDate,
+                    double weight,
+                    double height)
+        {
+            #region Condition check
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException("User name can't be empty or null.", nameof(name));
+            }
+
+            if (gender == null)
+            {
+                throw new ArgumentNullException("User gender can't be empty or null.", nameof(gender));
+            }
+
+            if (birthDate < DateTime.Parse("01.01.1900") || birthDate <= DateTime.Now)
+            {
+                throw new ArgumentException("Invalid birth date.", nameof (birthDate));
+            }
+
+            if (weight <= 0)
+            {
+                throw new ArgumentException("Weight can't be less than or equal to 0.", nameof(weight));
+            }
+            
+            if (height <= 0)
+            {
+                throw new ArgumentException("Height can't be less than or equal to 0.", nameof(height));
+            }
+            #endregion
+
+            Name = name;
+            Gender = gender;
+            BirthDate = birthDate;
+            Weight = weight;
+            Height = height;
+        }
+
+        public override string ToString()
+        {
+            return Name; 
+        }
+    }
+}
